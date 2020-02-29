cd3a03a732a39990e9a3d27703e19d0b17884bb3
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2020-02-29 12:02:44 +0100

    make net, nevali

--- pozar.go
+++ pozar.go
@@ -42,6 +42,15 @@ func contain(mapa map[int]int, v int) bool {
 	return false
 }
 
+func Acontain(mapa []int, v int) bool {
+	for _, test := range mapa {
+		if test == v {
+			return true
+		}
+	}
+	return false
+}
+
 func line(Px, Py, x, y int) (Fx, Fy int) {
 	// řeší X
 	if Px < x {
@@ -62,7 +71,129 @@ func line(Px, Py, x, y int) (Fx, Fy int) {
 	return
 }
 
+func revers(mapa [][]map[int]int, graph [][]Vertex, v int, step int) (prec []int) {
+	y := v / len(graph[0])
+	x := v % len(graph[0])
+	prec = make([]int, 0)
+	for Pv, Ps := range mapa[y][x] {
+		if Ps == (step - 1) {
+			Py := Pv / len(graph[0])
+			Px := Pv % len(graph[0])
+			if graph[Py][Px].cross >= 65 && graph[Py][Px].cross <= 90 {
+				prec = append(prec, Pv)
+				return
+			} else {
+				rev := revers(mapa, graph, Pv, step-1)
+				for _, r := range rev {
+					prec = append(prec, r)
+				}
+				return
+			}
+		}
+	}
+	return
+}
+
+func makeNet(rawGraph [][]Vertex) (graph map[int][]int) {
+	var AQueue, FQueue []int
+	var step int
+	graph = make(map[int][]int)
+	AQueue = make([]int, 0)
+	FQueue = make([]int, 0)
+	for i, v := range rawGraph[len(rawGraph)-1] {
+		if v.cross == '^' {
+			AQueue = append(AQueue, (len(rawGraph)-1)*len(rawGraph[0])+i)
+		}
+	}
+	mapa := make([][]map[int]int, len(rawGraph))
+	for i := range mapa {
+		mapa[i] = make([]map[int]int, len(rawGraph[0]))
+		for j := range mapa[i] {
+			mapa[i][j] = make(map[int]int) // if exist return step; odkud
+		}
+	}
+	for len(AQueue) > 0 {
+		//fmt.Println(AQueue)
+		for _, v := range AQueue {
+			y := v / len(rawGraph[0])
+			x := v % len(rawGraph[0])
+			s := rawGraph[y][x]
+			c := s.cross
+			//fmt.Println(rawGraph[y][x])
+			switch c {
+			case '^':
+				fallthrough
+			case '.':
+				for _, to := range s.next {
+					Fy := to / len(rawGraph[0])
+					Fx := to % len(rawGraph[0])
+					if to == -1 {
+						continue
+					}
+					_, err := mapa[Fy][Fx][v]
+					if err || contain(mapa[y][x], to) {
+						continue
+					}
+					//fmt.Print(to, ": ")
+					//fmt.Println(Fy, Fx)
+					FQueue = append(FQueue, to)
+					mapa[Fy][Fx][v] = step
+				}
+			case '=':
+				for Pv, Ps := range mapa[y][x] {
+					if Ps == (step - 1) {
+						Py := Pv / len(rawGraph[0])
+						Px := Pv % len(rawGraph[0])
+						for _, to := range s.next {
+							Fy := to / len(rawGraph[0])
+							Fx := to % len(rawGraph[0])
+							PredictX, PredictY := line(Px, Py, x, y)
+							if to == -1 {
+								continue
+							} else if PredictX != Fx || PredictY != Fy {
+								continue
+							}
+							_, err := mapa[Fy][Fx][v]
+							if err || contain(mapa[y][x], to) {
+								continue
+							}
+							//fmt.Print(to, ": ")
+							//fmt.Println(Fy, Fx)
+							FQueue = append(FQueue, to)
+							mapa[Fy][Fx][v] = step
+						}
+					}
+				}
+			case 'X':
+				fallthrough
+			case 'U':
+				fallthrough
+			case 'A':
+				fallthrough
+			case 'Y':
+				fallthrough
+			case 'D':
+				fallthrough
+			case '?':
+				fallthrough
+			case 'B':
+				prec := revers(mapa, rawGraph, v, step)
+				for _, p := range prec {
+					graph[v] = append(graph[v], p)
+				}
+			default:
+				continue
+			}
+		}
+		step++
+		AQueue = FQueue
+		FQueue = make([]int, 0)
+	}
+	return
+}
+
 func fire(graphRaw [][]Vertex, in [][]bool, monuments int) (out [][]bool) {
+	fmt.Println(makeNet(graphRaw))
 	out = make([][]bool, len(in))
 	for i := range out {
 		out[i] = make([]bool, monuments)
